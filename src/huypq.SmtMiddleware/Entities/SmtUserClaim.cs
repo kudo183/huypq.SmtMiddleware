957addf1395cf35d3ab9cd2be56a81b483a3d41f
kudo183@gmail.com <kudo183@gmail.com>
2017-05-05 15:01:41 +0700

    - add 3 Entities.

---
+++ src/huypq.SmtMiddleware/Entities/SmtUserClaim.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using huypq.SmtMiddleware;
+
+namespace huypq.SmtMiddleware.Entities
+{
+    public partial class SmtUserClaim : IUserClaim
+    {
+        public SmtUserClaim()
+        {
+
+        }
+
+        public int ID { get; set; }
+        public int UserID { get; set; }
+        public string Claim { get; set; }
+        public int TenantID { get; set; }
+        public long LastUpdateTime { get; set; }
+        public long CreateTime { get; set; }
+
+        public SmtUser UserIDNavigation { get; set; }
+        public SmtTenant TenantIDNavigation { get; set; }
+		
+
+    }
+}
