35ef4f57dede7b15f4a1c302f795c743618e9f52
kudo183@gmail.com <kudo183@gmail.com>
2017-07-20 18:29:54 +0700

    - upgrade to VS 2017.

--- huypq.SmtMiddleware/SmtEntityBaseController.cs
+++ huypq.SmtMiddleware/SmtEntityBaseController.cs
@@ -6,7 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using huypq.SmtShared;
 using huypq.SmtShared.Constant;
-using QueryBuilder;
+using huypq.QueryBuilder;
 using huypq.SmtMiddleware.Entities;
 
 namespace huypq.SmtMiddleware
